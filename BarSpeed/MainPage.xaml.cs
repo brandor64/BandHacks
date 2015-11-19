cbd86f1bc96da79d5d6092472a646d8368682375
brandor <millerbp@gmail.com>
2015-11-18 17:40:32 -0800

    Added connection and accelerometer startup

--- BarSpeed/MainPage.xaml.cs
+++ BarSpeed/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Band;
+using Microsoft.Band.Sensors;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,10 +24,14 @@ namespace BarSpeed
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private IBandClient bandClient;
+
         public MainPage()
         {
             this.InitializeComponent();
+
             ConnectToBand();
+            PopulateSupportedIntervals();
         }
 
         private async void ConnectToBand()
@@ -36,15 +41,56 @@ namespace BarSpeed
             //connect to the Band to get a new BandClient object
             try
             {
-                using (IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
+                using (bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
                 {
-                    //do work after successful connect
+                    // check current sensor consent
+                    if (bandClient.SensorManager.Accelerometer.GetCurrentUserConsent() != UserConsent.Granted)
+                    {
+                        // user hasn't consented, request consent
+                        await bandClient.SensorManager.Accelerometer.RequestUserConsentAsync();
+                    }
                 }
             }
             catch (BandException ex)
             {
                 // handle Band connection exception
             }
+
+            // start the accelerometer sensor
+            try
+            {
+                await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
+            }     
+            catch(BandException ex)
+            {
+                // handle accelerometer exception
+                throw ex;
+            }
+        }
+
+        private void PopulateSupportedIntervals()
+        {
+            // get a list of available reporting intervals 
+            IEnumerable<TimeSpan> supportedAccelerometerReportingIntervals = bandClient.SensorManager.Accelerometer.SupportedReportingIntervals;
+            foreach (var ri in supportedAccelerometerReportingIntervals)
+            {
+                // do work with each reporting interval (i.e., add them to a list in the UI) 
+                greetingOutput.Text = ri.Seconds.ToString();
+            }
+
+            // set the reporting interval 
+            bandClient.SensorManager.Accelerometer.ReportingInterval = supportedAccelerometerReportingIntervals.GetEnumerator().Current;
+
+            // hook up to the Heartrate sensor ReadingChanged event 
+            bandClient.SensorManager.HeartRate.ReadingChanged += (sender, args) =>  
+            {
+                // do work when the reading changes (i.e., update a UI element) 
+            }; 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            greetingOutput.Text = "Hi, " + nameInput.Text;
         }
     }
 }
